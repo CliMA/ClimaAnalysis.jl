496e7ff200a5878bac66f204f937effebed4dd84
Haakon Ludvig Langeland Ervik <45243236+haakon-e@users.noreply.github.com>
2024-04-17 11:51:19 -0700

    helpful error messages
    see #15

--- src/Visualize.jl
+++ src/Visualize.jl
@@ -18,4 +18,14 @@ function plot! end
 
 function heatmap2D_on_globe! end
 
+function __init__()
+    Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
+        if exc.f in [heatmap2D!, sliced_heatmap!, heatmap!, line_plot1D!, sliced_line_plot!, line_plot!, sliced_plot!, plot!, heatmap2D_on_globe!]
+            if isempty(methods(exc.f))
+                print(io, "\n$(exc.f) has no methods, yet. Makie has to be loaded for the plotting extension to be activated. Run `using Makie`, `using CairoMakie`, `using GLMakie` or any other package that also loads Makie.")
+            end
+        end
+    end
+end
+
 end
