86893872b665b20d40eb197f8ba71246dbfc6974
Gabriele Bozzola <gbozzola@caltech.edu>
2024-08-27 16:31:35 -0700

    Remove extra line

--- src/Var.jl
+++ src/Var.jl
@@ -193,7 +193,6 @@ end
 
 Return whether the given `var`iable has an altitude dimension that is 1D.
 
-
 When topography is present, the altitude dimension in the output variable is typically
 multidimensional. The dimensions are (X, Y, Z), where (X, Y) are the horizontal dimensions.
 In this case, `dims["z"]` is essentially a map that identifies the physical altitude of the
