9cea8db593a34dae9ee728aa3b1a5103c65a0610
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2024-09-05 14:05:29 -0700

    Uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,16 @@ using SafeTestsets
 using Test
 
 #! format: off
-# @safetestset "Aqua" begin @time include("aqua.jl") end
-# @safetestset "Docstrings" begin @time include("doctest.jl") end
-# @safetestset "Format" begin @time include("format.jl") end
+@safetestset "Aqua" begin @time include("aqua.jl") end
+@safetestset "Docstrings" begin @time include("doctest.jl") end
+@safetestset "Format" begin @time include("format.jl") end
 
-# @safetestset "Utils" begin @time include("test_Utils.jl") end
-# @safetestset "SimDir" begin @time include("test_Sim.jl") end
-# @safetestset "Atmos" begin @time include("test_Atmos.jl") end
-# @safetestset "OutputVar" begin @time include("test_Var.jl") end
-# @safetestset "MakieExt" begin @time include("test_MakieExt.jl") end
+@safetestset "Utils" begin @time include("test_Utils.jl") end
+@safetestset "Numerics" begin @time include("test_Numerics.jl") end
+@safetestset "SimDir" begin @time include("test_Sim.jl") end
+@safetestset "Atmos" begin @time include("test_Atmos.jl") end
+@safetestset "OutputVar" begin @time include("test_Var.jl") end
+@safetestset "MakieExt" begin @time include("test_MakieExt.jl") end
 @safetestset "GeoMakieExt" begin @time include("test_GeoMakieExt.jl") end
 #! format: on
 
