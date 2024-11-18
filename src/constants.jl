f8edf1135257a9c8b2bd3e52550807f58ca6cb99
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2024-11-18 15:16:32 -0800

    Replace land and sea masks

--- src/constants.jl
+++ src/constants.jl
@@ -1,23 +1,58 @@
-"""
-The variable LAND_MASK is a filepath to a NCDataset whose dimensions are latitude and
-longitude and data are ones and zeros. The zeros indicate land and ones indicate everything
-else.
-
-The land mask is generated from the ETOPO2022 dataset at 60 arc-second resolutions
-(https://www.ncei.noaa.gov/products/etopo-global-relief-model). The dimensions latitude and
-longitude are sampled every 16th element to thin out the dimensions. Any value for z greater
-than 0 is assigned 0.0 and all other values for z is assigned 1.0.
-"""
-LAND_MASK = joinpath(@__DIR__, "..", "masks", "land_mask16.nc")
+import NCDatasets
+import OrderedCollections: OrderedDict
+import Artifacts
 
 """
-The variable OCEAN_MASK is a filepath to a NCDataset whose dimensions are latitude and
-longitude and data are 1s and 0s. The zeros indicate ocean and ones indicate everything
-else.
-
-The land mask is generated from the ETOPO2022 dataset at 60 arc-second resolutions
-(https://www.ncei.noaa.gov/products/etopo-global-relief-model). The dimensions latitude and
-longitude are sampled every 16th element to thin out the dimensions. Any value for z less
-than or equal to 0 is assigned 0.0 and all other values for z is assigned 1.0.
+The variable `LAND_MASK` is an `OutputVar` whose data consists of ones and NaNs. The ones
+indicate ocean and NaNs indicate the land.
 """
-OCEAN_MASK = joinpath(@__DIR__, "..", "masks", "ocean_mask16.nc")
+const LAND_MASK = begin
+    path = joinpath(
+        Artifacts.artifact"landsea_mask_30arcseconds",
+        "landsea_mask.nc",
+    )
+    ds = NCDatasets.NCDataset(path)
+    # Taking every 16th element corresponds to roughly 0.1 degree resolution
+    # This is roughly 0.05 GB in size
+    # Every 8th element -> ~0.25 GB
+    # Every 4th element -> ~1 GB
+    lon = ds["lon"][begin:16:end]
+    lat = ds["lat"][begin:16:end]
+    data = ds["landsea"][begin:16:end, begin:16:end]
+    close(ds)
+
+    dims = OrderedDict(["lon" => lon, "lat" => lat])
+    attribs = Dict("long_name" => "Land sea mask")
+    dim_attribs = OrderedDict([
+        "lat" => Dict("units" => "deg"),
+        "lon" => Dict("units" => "deg"),
+    ])
+    var = OutputVar(attribs, dims, dim_attribs, data)
+    var = replace(var, 0.0 => 1.0, 1.0 => NaN)
+end
+
+"""
+The variable `OCEAN_MASK` is an `OutputVar` whose data consists of ones and NaNs. The ones
+indicate land and NaNs indicate the ocean.
+"""
+const OCEAN_MASK = begin
+    path = joinpath(
+        Artifacts.artifact"landsea_mask_30arcseconds",
+        "landsea_mask.nc",
+    )
+    ds = NCDatasets.NCDataset(path)
+    # See comment in LAND_MASK for why 16 is chosen
+    lon = ds["lon"][begin:16:end]
+    lat = ds["lat"][begin:16:end]
+    data = ds["landsea"][begin:16:end, begin:16:end]
+    close(ds)
+
+    dims = OrderedDict(["lon" => lon, "lat" => lat])
+    attribs = Dict("long_name" => "Land sea mask")
+    dim_attribs = OrderedDict([
+        "lat" => Dict("units" => "deg"),
+        "lon" => Dict("units" => "deg"),
+    ])
+    var = OutputVar(attribs, dims, dim_attribs, data)
+    var = replace(var, 0.0 => NaN)
+end
