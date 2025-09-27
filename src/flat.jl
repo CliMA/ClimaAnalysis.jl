5f794ab2ca6f773f0a043fb08a0319b24f421c5b
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2025-09-26 17:00:31 -0700

    Fix type instability in unflatten

--- src/flat.jl
+++ src/flat.jl
@@ -4,7 +4,7 @@ export flatten, unflatten, flatten_dim_order, flattened_length
     Representing the metadata of an `OutputVar` and contain all the necessary
     information to reconstruct an `OutputVar`.
 """
-struct Metadata{T <: AbstractArray, B, C, FT}
+struct Metadata{T <: AbstractArray, B, C, FT, N}
     "Attributes associated to this variable, such as short/long name"
     attributes::Dict{String, B}
 
@@ -15,7 +15,7 @@ struct Metadata{T <: AbstractArray, B, C, FT}
     dim_attributes::OrderedDict{String, C}
 
     "Order of dimensions when flattening the data of the OutputVar"
-    ordered_dims::Tuple{Vararg{String}}
+    ordered_dims::NTuple{N, String}
 
     "Bit mask of the dropped values when flattening"
     drop_mask::BitVector
