823c54d3a8033b65c9e5f9c888438eb7616b933c
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2025-12-12 15:10:18 -0800

    Use ParallelTestRunner

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,6 @@
-using SafeTestsets
-using Test
+using ClimaAnalysis
+using ParallelTestRunner
 
-#! format: off
-@safetestset "Aqua" begin @time include("aqua.jl") end
-@safetestset "Docstrings" begin @time include("doctest.jl") end
-@safetestset "Format" begin @time include("format.jl") end
-
-@safetestset "Utils" begin @time include("test_Utils.jl") end
-@safetestset "Numerics" begin @time include("test_Numerics.jl") end
-@safetestset "SimDir" begin @time include("test_Sim.jl") end
-@safetestset "Catalog" begin @time include("test_Catalog.jl") end
-@safetestset "Atmos" begin @time include("test_Atmos.jl") end
-@safetestset "Leaderboard" begin @time include("test_Leaderboard.jl") end
-@safetestset "Template" begin @time include("test_Template.jl") end
-@safetestset "OutputVar" begin @time include("test_Var.jl") end
-@safetestset "Selectors" begin @time include("test_outvar_selectors.jl") end
-@safetestset "FlatVar" begin @time include("test_flat.jl") end
-@safetestset "MakieExt" begin @time include("test_MakieExt.jl") end
-@safetestset "GeoMakieExt" begin @time include("test_GeoMakieExt.jl") end
-#! format: on
+runtests(ClimaAnalysis, ARGS)
 
 nothing
